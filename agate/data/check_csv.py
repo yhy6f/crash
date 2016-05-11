710ce3190f4c3ad1307a6c8ba81db5baa4c1bde2
Jasmine Ye Han <jasmine.han@foxmail.com>
2016-05-11 15:20:07 -0500

    more cleaning and analyslysis in agate

--- agate/data/check_csv.py
+++ agate/data/check_csv.py
@@ -1,18 +1,29 @@
 import csv
 
-output = []
+with open('crashes2011.csv') as f:
+    reader = csv.reader(f, delimiter =",")
+    i = 0
+    for row in reader:
 
-with open('crashes2008.csv') as f:
-    reader = csv.reader(f)
-    header =reader.next()
-	##.next()method is telling python to start from the first line of reader 
-    header.pop(-1)
+        i = i+1;
+        if len(row) > 49:
+            print i
 
-    for line in reader:
-        line.pop(-1)
-        output.append(line)
 
-with open('crashes2008_clean.csv') as f:
-    writer = csv.writer(f)
-    writer.writerow(header)
-    writer.writerows(output)
+# import csv
+
+# output = []
+
+# with open('crashes2008.csv') as f:
+#     reader = csv.reader(f)
+#     header =reader.next()
+#   ##.next()method is telling python to start from the first line of reader 
+#     header.pop(-1)
+
+#     for line in reader:
+#         line.pop(-1)
+#         output.append(line)
+# with open('crashes2008_clean.csv') as f:
+#     writer = csv.writer(f)
+#     writer.writerow(header)
+#     writer.writerows(output)
